1cea2824c9088332a4da22f33c9ab86c07391edb
Rubidubiscarlett <rubidubiscarlett@gmail.com>
2017-10-27 12:29:44 -0500

    finalprojectclue

---
+++ final_project_clues/README.md
@@ -0,0 +1,6 @@
+## Pistas sobre proyecto final.
+
+1. repaso sobre metodos arreglos
+2. metodos para los strings
+3. Ascii
+4 .for, condicionales(if, if else);
---
+++ final_project_clues/js/app.js
@@ -0,0 +1,8 @@
+var testArray = [];
+for(var i=0; i<=10; i+=1){
+testArray.push(i);
+}
+console.log(testArray);
+//eliminar el primer elemento del arreglo
+testArray.shift();
+console.log(testArray);
